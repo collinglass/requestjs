d8111073c1e037411466704ae96511e76bbd369b
Collin Glass <collinglass@gmx.com>
2014-08-16 09:20:33 -0400

    0.0.2 commitment

--- README.md
+++ README.md
@@ -10,10 +10,16 @@ var api = 'http://localhost:1337/api'
 //	Returns api config
 function getConfig(data){
 	var result = data || {};
-	return request(api+"/config", "GET").then(function(response) { return response.data.result });
+	return request(api+"/config", "GET", true).then(function(response) { return response.data.result });
 };
 ```
 
 ## Work in progress.
 
 Currently only tested with GET requests.
+
+### Releases
+
+0.0.1 - All json
+
+0.0.2 - Added variable to return json or raw
--- request.js
+++ request.js
@@ -6,27 +6,35 @@
 // @author Collin Glass <collinglass@gmx.com>
 // @copyright 2014 Collin Glass
 // @license BSD-3 <https://raw.github.com/collinglass/request/master/LICENSE>
-// @version 0.0.1
+// @version 0.0.2
 //
 
-function request(route, method) {
+function request(route, method, json) {
 	return new Promise(function(resolve, reject){
 		var xmlhttp = new XMLHttpRequest();
 
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState == 4 ) {
 				if (xmlhttp.status == 200) {
-					resolve((eval("(" + xmlhttp.responseText + ")")))
+					if (json == true)
+						resolve((eval("(" + xmlhttp.responseText + ")")))
+					else
+						resolve(xmlhttp.responseText)
 				} else if (xmlhttp.status == 400) {
-					reject((eval("(" + xmlhttp.responseText + ")")))
+					if (json == true)
+						reject((eval("(" + xmlhttp.responseText + ")")))
+					else
+						reject(xmlhttp.responseText)
 				} else {
-					reject((eval("(" + xmlhttp.responseText + ")")))
+					if (json == true)
+						reject((eval("(" + xmlhttp.responseText + ")")))
+					else
+						reject(xmlhttp.responseText)
 				}
 			}
 		}
 
 		xmlhttp.open(method, route, true);
 		xmlhttp.send();
-		
 	});
 }
